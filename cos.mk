c9b018f8145c02ba9429edb0fea8989e6b884876
KagamiTaiga <dinix.hot@gmail.com>
2017-05-14 22:55:15 -0300

    Update and rename cyanide_victara.mk to cos.mk

--- cos.mk
+++ cos.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := cyanide_victara
+PRODUCT_NAME := cos_victara
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
