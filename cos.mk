fd5e4c2253b3e5fe8459ad8fb1ac6a041e532c54
KagamiTaiga <dinix.hot@gmail.com>
2017-06-10 19:57:36 +0000

    Create cos.mk

--- cos.mk
+++ cos.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := tipsy_victara
+PRODUCT_NAME := cos_victara
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
